34c356fba5e985d5000c2ab8a6429019b6439671
tharun <madoththarun22@gamail.com>
2024-03-15 12:25:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import MobileViewFooterCard from "./Components/MobileViewFooterCard";
+import MobishaalaCardDetails from "./Components/MobishaalaCardDetails";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="page">
+      <MobishaalaCardDetails />
+      <MobileViewFooterCard />
+    </div>
+  );
+}
+
+export default App;
