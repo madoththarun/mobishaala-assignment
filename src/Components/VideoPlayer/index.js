b6fb1ed64cf4e6af71458d3b07e9c659c2f646d7
Madoth Tharun <123082304+madoththarun@users.noreply.github.com>
2024-03-15 13:24:12 +0530

    Update index.js

--- src/Components/VideoPlayer/index.js
+++ src/Components/VideoPlayer/index.js
@@ -1,12 +1,20 @@
 import ReactPlayer from "react-player";
-
+import "./index.css";
 const VideoPlayer = () => (
-  <ReactPlayer
-    width="100%"
-    height="100%"
-    style={{ borderRadius: "10px", overflow: "hidden" }}
-    url="https://youtu.be/jg5d59ligW4?si=K_7Nzpz6aVNM7PA-"
-  />
+  <div className="video-player-container">
+    <div>
+      <ReactPlayer
+        width="100%"
+        height="100%"
+        style={{ borderRadius: "10px", overflow: "hidden" }}
+        url="https://youtu.be/jg5d59ligW4?si=K_7Nzpz6aVNM7PA-"
+      />
+    </div>
+    <div className="button-container">
+      <button type="button" className="btn text-center">
+        Buy
+      </button>
+    </div>
+  </div>
 );
-
 export default VideoPlayer;
